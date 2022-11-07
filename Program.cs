c8119e8890a052ce41c72100dfa5504858dd117f
namubar <mubarakshin.nail@tatar.ru>
2022-11-07 21:17:54 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+int rez = 1;
+int power(int a, int b)
+{
+    //int rez = 1;
+    for (int k = 1; k <= b; k = k + 1)
+    {
+        rez = rez * a;
+    }
+    return rez;
+}
+power(a, b);
+Console.WriteLine("Результат: " + rez);
